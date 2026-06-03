6561dcddf0a60d6ad54f04e69d8255b2798c1ced
Christopher Hercules <hi65531@mail.harpercollege.edu>
2026-06-03 16:00:35 -0500

    Initial commit

---
+++ helloWorldLab.cpp
@@ -0,0 +1,13 @@
+//Programmer:Christopher Hercules 
+//Purpose: making sure everything works
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+
+    return 0;
+}
+
